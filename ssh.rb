5338061ed3ff11fa0f3745a075d7d00c178f9a58
Marius Vollmer <mvollmer@redhat.com>
2018-10-19 13:46:24 +0300

    WIP - be a server

--- ssh.rb
+++ ssh.rb
@@ -1,22 +1,44 @@
 require 'net/ssh'
 
-Net::SSH.start("localhost", "root", :password => "foobar" ) do |ssh|
-  channel = ssh.open_channel do |ch|
-    ch.exec("/bin/bash") do |ch, success|
-      raise "could not execute command" unless success
+def ssh_shell(socket)
+  Net::SSH.start("localhost", "root", :password => "foobar" ) do |ssh|
+    channel = ssh.open_channel do |ch|
+      ch.exec("bash") do |ch, success|
+        raise "could not execute command" unless success
 
-      ch.on_data do |ch2, data|
-        print("0: ", data, "\n")
+        socket.extend(Net::SSH::BufferedIo)
+        ssh.listen_to(socket)
+
+        ch.on_process do
+          print("P", socket.available, " ", socket.closed?, "\n");
+          if socket.available > 0
+            ch.send_data(socket.read_available)
+          end
+          if socket.closed?
+            ch.close
+          end
+        end
+
+        ch.on_data do |ch2, data|
+          print("0: ", data, "\n")
+          socket.enqueue(data)
+        end
+
+        ch.on_extended_data do |ch2, type, data|
+          print("extended\n")
+          print(type, ": ", data, "\n")
+        end
+
       end
 
-      ch.on_extended_data do |ch2, type, data|
-        print(type, ": ", data, "\n")
-      end
-
-      ch.send_data("ls \n")
     end
 
+    channel.wait
+    socket.close
+    print("EXIT\n")
   end
-
-  channel.wait
 end
+
+listen_socket = TCPServer.open(1234)
+socket = listen_socket.accept
+ssh_shell(socket)
